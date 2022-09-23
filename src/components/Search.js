3c6d8e169cfcf0498b988550bf23727cdde39f8f
brs14shn <brsshn292929@gmail.com>
2022-09-24 00:04:59 +0300

    Search updated

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,32 @@
 import React from 'react'
+//* icons
+import SearchIcon from '@mui/icons-material/Search';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
 
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 const Search = () => {
+
+  const [input,setInput]=React.useState("")
+
+
   return (
-    <div>Search</div>
+    <form>
+      <div>
+        <SearchIcon/>
+        <input 
+        type="text" 
+        value={input}
+        onChange={(e)=>setInput(e.target.value)}
+        />
+        <KeyboardVoiceIcon/>
+      </div>
+      <Stack spacing={2} direction="row">
+      <Button type="submit" variant="outlined">Google Search</Button>
+      <Button variant="outlined">I am feeling lucky</Button>
+    </Stack>
+
+    </form>
   )
 }
 
